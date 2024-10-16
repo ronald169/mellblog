1287717fc20176aede85719b0fa5b999b832930e
melltrust <ronalsgatsi@gmail.com>
2024-10-16 13:14:23 +0100

    Setup authentification system

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
 Volt::route('/', 'users.index');
+
+Route::middleware('guest')->group(function () {
+    Volt::route('/register', 'auth.register')->name('register');
+    Volt::route('/login', 'auth.login')->name('login');
+    Volt::route('/forgot-password', 'auth.forgot-password')->name('forgot-password');
+    Volt::route('/reset-password', 'auth.reset-password')->name('reset-password');
+});
